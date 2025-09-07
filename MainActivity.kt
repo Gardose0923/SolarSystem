735c49383b147a5b9c9fbc5ba019aa99b9ba9cf8
Gardose0923 <159867793+Gardose0923@users.noreply.github.com>
2025-09-07 21:22:05 +0800

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,18 @@
+package com.planets
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Load the QuestionsFragment initially
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, QuestionsFragment())
+                .commit()
+        }
+    }
+}
